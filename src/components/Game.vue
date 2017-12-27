0cd50de1691c70f158a09040bd8de3d242477b80
Durban-Designer <royce.birnbaum@gmail.com>
2017-12-27 16:37:17 -0700

    Made the game work

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="main">
-    <threethousand v-if="!classic" v-on:saveGame="saveGame" v-on:turnChange="turnChange" :year="gameData.year" :starved="gameData.starved" :immigrants="gameData.immigrants" :population="gameData.population" :land="gameData.land" :harvest="gameData.harvest" :bushelsPerAcre="gameData.bushelsPerAcre" :rats="gameData.rats" :store="gameData.store" :landRate="gameData.landRate"></threethousand>
-    <classicmode v-if="classic" v-on:saveGame="saveGame" v-on:turnChange="turnChange" :year="gameData.year" :starved="gameData.starved" :immigrants="gameData.immigrants" :population="gameData.population" :land="gameData.land" :harvest="gameData.harvest" :bushelsPerAcre="gameData.bushelsPerAcre" :rats="gameData.rats" :store="gameData.store" :landRate="gameData.landRate"></classicmode>
+    <threethousand v-if="!classic && !victory" v-on:saveGame="saveGame" v-on:turnChange="turnChange" :year="gameData.year" :starved="gameData.starved" :immigrants="gameData.immigrants" :population="gameData.population" :land="gameData.land" :harvest="gameData.harvest" :bushelsPerAcre="gameData.bushelsPerAcre" :rats="gameData.rats" :store="gameData.store" :landRate="gameData.landRate"></threethousand>
+    <classicmode v-if="classic && !victory" v-on:saveGame="saveGame" v-on:turnChange="turnChange" :year="gameData.year" :starved="gameData.starved" :immigrants="gameData.immigrants" :population="gameData.population" :land="gameData.land" :harvest="gameData.harvest" :bushelsPerAcre="gameData.bushelsPerAcre" :rats="gameData.rats" :store="gameData.store" :landRate="gameData.landRate"></classicmode>
+    <div v-if="victory">
+      You win!!
+    </div>
   </div>
 </template>
 
@@ -21,9 +24,11 @@ export default {
   },
   data () {
     return {
+      victory: false,
       gameData: {
         year: 1,
         starved: 0,
+        starvedTotal: 0,
         immigrants: 5,
         population: 100,
         land: 1000,
@@ -32,6 +37,7 @@ export default {
         harvest: 2000,
         store: 2800,
         landRate: 26,
+        plague: false,
         classicMode: false,
         gameDifficulty: 'easy',
         name: '',
@@ -41,6 +47,41 @@ export default {
   },
   methods: {
     turnChange: function (inputs) {
+      this.gameData.year++
+      if (Math.floor(Math.random() * 20) >= 17) {
+        this.gameData.plague = true
+        this.gameData.population = this.gameData.population - (this.gameData.population * Math.floor(Math.random() * 0.33))
+      }
+      else {
+        this.gameData.plague = false
+      }
+      if (inputs.bushelFeed / 20 >= this.gameData.population) {
+        this.gameData.starved = 0
+        this.gameData.store = this.gameData.store - inputs.bushelFeed
+      }
+      else {
+        this.gameData.starved = this.gameData.population - (inputs.bushelFeed / 20)
+        this.gameData.starvedTotal = this.gameData.starvedTotal + this.gameData.starved
+        if (this.gameData.starvedTotal >= 50) {
+          this.victory = true
+        }
+        this.gameData.population = this.gameData.population - this.gameData.starved
+        this.gameData.store = this.gameData.store - inputs.bushelFeed
+      }
+      this.gameData.immigrants = Math.floor(Math.random() * 10)
+      this.gameData.population = this.gameData.population + this.gameData.immigrants
+      if (inputs.landBuy !== 0) {
+        this.gameData.land = parseInt(this.gameData.land) + parseInt(inputs.landBuy)
+      }
+      this.gameData.landRate = Math.floor(Math.random() * 9) + 17
+      this.gameData.bushelsPerAcre = (Math.floor(Math.random() * 8) + 1)
+      this.gameData.harvest = inputs.planted * this.gameData.bushelsPerAcre
+      this.gameData.store = this.gameData.store + this.gameData.harvest - inputs.planted
+      this.gameData.rats = Math.floor((Math.random() * 0.2) * this.gameData.store)
+      this.gameData.store = this.gameData.store - this.gameData.rats
+      if (this.gameData.year >= 11) {
+        this.victory = true
+      }
     },
     saveGame: function (name) {
       this.gameData.name = name
